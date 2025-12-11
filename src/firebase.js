ab9db71131afa386ea4ee2cee1276a5a429a9971
Payton Pleasant <paytonpleasant@Paytons-MacBook-Air.local>
2025-12-11 05:54:53 -0600

    Secure firebase config with env vars

--- src/firebase.js
+++ src/firebase.js
@@ -3,13 +3,13 @@ import { getAuth } from "firebase/auth";
 import { getFirestore, setLogLevel } from "firebase/firestore";
 
 const firebaseConfig = {
-    apiKey: "AIzaSyCAEpgsGYDAylUk6zsPCLs1pYWP_GEw3O0",
-    authDomain: "fruitopia-1bba8.firebaseapp.com",
-    projectId: "fruitopia-1bba8",
-    storageBucket: "fruitopia-1bba8.firebasestorage.app",
-    messagingSenderId: "440594841174",
-    appId: "1:440594841174:web:5ae7700eb650ae212e85f7",
-    measurementId: "G-BTS1B9QSB3"
+    apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
+    authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
+    projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
+    storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
+    appId: import.meta.env.VITE_FIREBASE_APP_ID,
+    measurementId: import.meta.env.VITE_FIREBASE_MEASUREMENT_ID
 };
 
 const app = initializeApp(firebaseConfig);
